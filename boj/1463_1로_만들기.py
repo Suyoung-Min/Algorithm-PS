da58d97a46520a59cfccb48550c8ae1a09176f1f
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2021-07-15 15:57:18 +0900

    Update 1463_1로_만들기.py

--- boj/1463_1로_만들기.py
+++ boj/1463_1로_만들기.py
@@ -6,11 +6,11 @@ dp[3]=1
     
 for i in range(4,n+1):
     if i%6 == 0:
-        dp[i] = min([dp[i-1],dp[int(i/2)],dp[int(i/3)]])+1
+        dp[i] = min([dp[i-1],dp[i//2],dp[i//3]])+1
     elif i%3 == 0:
-        dp[i] = min([dp[i-1],dp[int(i/3)]])+1
+        dp[i] = min([dp[i-1],dp[i//3]])+1
     elif i%2 == 0:
-        dp[i] = min([dp[i-1],dp[int(i/2)]])+1
+        dp[i] = min([dp[i-1],dp[i//2]])+1
     else:
         dp[i]=dp[i-1]+1
         
