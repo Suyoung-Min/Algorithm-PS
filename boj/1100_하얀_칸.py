eb193af57cabcae9d62a3a886b6a0c65f925edc0
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2021-07-21 23:56:38 +0900

    Update 1100_하얀_칸.py

--- boj/1100_하얀_칸.py
+++ boj/1100_하얀_칸.py
@@ -3,7 +3,7 @@ for i in range(8):
    board[i]=input()
 a=0
 for i in range(8):
-   for j in range(8):
+   for j in range(i%2,8,2):
       if (i+j)%2 == 0 and board[i][j] == 'F':
          a+=1
 print(a)
