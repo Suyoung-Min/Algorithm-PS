210e74c37a11419130062f6be85e902a7341842b
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2021-07-24 00:42:20 +0900

    Update 파이썬 문법 & 라이브러리.py

--- basic_algorithm_template/파이썬 문법 & 라이브러리.py
+++ basic_algorithm_template/파이썬 문법 & 라이브러리.py
@@ -1,3 +1,16 @@
 import sys
+import heapq
 
-str1 = sys.stdin.readline().rstrip()
+str1 = sys.stdin.readline().rstrip() #input 빠르게 받기
+
+heap=[]
+heapq.heappush(heap,10)
+heapq.heappush(heap,1)
+heapq.heappush(heap,3)
+heapq.heappush(heap,4)
+print(heap)
+print(heapq.heappop(heap))
+"""
+min-heap library heapq
+target 은 list 형태로 저장
+"""
