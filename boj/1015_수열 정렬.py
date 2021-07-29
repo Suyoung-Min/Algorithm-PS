e391c115cb9f8e9e2aae754abf88170924447e9b
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2021-07-30 02:37:40 +0900

    Update 1015_수열 정렬.py

--- boj/1015_수열 정렬.py
+++ boj/1015_수열 정렬.py
@@ -8,3 +8,14 @@ for i in range(len(b)):
             print(j,end=' ')
             visited[j] = True
             break
+            
+##############################################
+n = int(input())
+t = list(map(int, input().split()))
+s_li = sorted(t)
+li = []
+for i in range(n):
+    idx = s_li.index(t[i])
+    li.append(idx)
+    s_li[idx] = -1
+print(*li)
