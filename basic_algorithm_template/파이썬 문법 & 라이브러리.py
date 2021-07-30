0fc78cd1b5ae7d796d2d66d53fd5c4d4dfee8f21
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2021-07-30 22:56:40 +0900

    Update 파이썬 문법 & 라이브러리.py

--- basic_algorithm_template/파이썬 문법 & 라이브러리.py
+++ basic_algorithm_template/파이썬 문법 & 라이브러리.py
@@ -1,5 +1,7 @@
 import sys
 import heapq
+import math
+from collections import deque
 
 str1 = sys.stdin.readline().rstrip() #input 빠르게 str 받기 & 개행문자 제거 
 list1 = map(int,sys.stdin.readline().split()) # 여러개 정수를 리스트로 받기
@@ -15,3 +17,9 @@ print(heapq.heappop(heap))
 min-heap library heapq
 target 은 list 형태로 저장
 """
+
+print( math.ceil(12.2) ) # 13 올림
+print( math.floor(12.2) ) # 12 내림
+print( math.trunc(12.2) ) # 12 버림
+
+print( round(0.4666 , 2) ) # 0.47 반올림 & 자릿수
