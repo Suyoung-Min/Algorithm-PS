e0dc720eb9b1175923589d9e6eb543b90a39c81f
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2021-08-31 01:14:41 +0900

    Update 14925_목장 건설하기.cpp

--- boj_c++/14925_목장 건설하기.cpp
+++ boj_c++/14925_목장 건설하기.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-//시간이 오래 걸림, 다른 방식의 dp는?
+//시간이 오래 걸림, 다른 방식의 dp는? => 일일이 하는 것이 아닌 타겟 좌표의 2x2행렬만 확인하면 됨 by min function
 
 int m,n;
 int d[1001][1001]={0,};
@@ -61,6 +61,60 @@ int main(){
                 t++;
             }
             dp[i][j]=t;
+        }
+    }
+
+    ans=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(dp[i][j] > ans) ans = dp[i][j];
+        }
+    }
+
+    cout<<ans<<'\n';
+}
+
+//888ms
+##########################################
+//68ms
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int m,n;
+int d[1001][1001]={0,};
+int dp[1001][1001]={0,};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>m>>n;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++)
+            cin>>d[i][j];
+    }
+
+
+    for(int i=0;i<n;i++){
+        if(d[0][i] == 0)
+            dp[0][i]=1;
+    }
+    for(int i=0;i<m;i++){
+        if(d[i][0] == 0)
+            dp[i][0]=1;
+    }
+
+    int t;
+    bool break_flag;
+    int ans=0;
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(d[i][j] == 0){
+                dp[i][j]=min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]})+1;
+            }
         }
     }
 
