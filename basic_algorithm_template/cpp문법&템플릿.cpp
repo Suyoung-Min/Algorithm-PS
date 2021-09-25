f872a3fd258c6fee733d80f8b2e53339b6053577
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2021-09-26 02:38:43 +0900

    Update cpp문법&템플릿.cpp

--- basic_algorithm_template/cpp문법&템플릿.cpp
+++ basic_algorithm_template/cpp문법&템플릿.cpp
@@ -15,6 +15,36 @@ int gcd(int a,int b){
 유클리드 호제법을 이용한 최대공약수 greater common dividor 계산
 */
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin>>n;
+
+    priority_queue<int,vector<int>,greater<int>> pq;
+
+    while(n--){
+        cin>>x;
+        if(x == 0){
+            if(!pq.empty()){
+                cout<<pq.top()<<'\n';
+                pq.pop();
+            }else{
+                cout<<"0\n";
+            }
+        }else if(x > 0){
+            pq.push(x);
+        }
+    }
+
+    return 0;
+}
+/*
+우선순위 큐
+오름차순 greater
+*/
+
+int main(){
 
     int a,b;
     cin>>a>>b;
