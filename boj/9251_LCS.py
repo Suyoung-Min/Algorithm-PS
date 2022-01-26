08e8b76e9f321950239c567145a7b08fdf385a07
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2022-01-27 02:13:31 +0900

    Create 9251_LCS.py

---
+++ boj/9251_LCS.py
@@ -0,0 +1,8 @@
+a = input()
+b = input()
+dp = [[0]*(len(b)+1) for _ in range(len(a)+1)]
+for i in range(len(a)):
+    for j in range(len(b)):
+        if a[i] == b[j]: dp[i+1][j+1] = dp[i][j] + 1
+        else: dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+print(dp[len(a)][len(b)])
