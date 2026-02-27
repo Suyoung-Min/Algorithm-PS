4172658e9d6d55421c8aae12747cfc1bac8d1780
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-02-27 18:10:01 +0900

    Title: 십자 모양의 지속적 폭발, Time: 77ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260227/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.py
@@ -0,0 +1,54 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+commands = [int(input())-1 for _ in range(m)]
+
+# Please write your code here.
+
+def cross_bomb(ty,tx):
+    
+    t = grid[ty][tx]
+    
+    for x in range(tx-t+1, tx+t-1+1):
+        if x < 0 or x >= n: continue
+        
+        grid[ty][x] = 0
+        
+    for y in range(ty-t+1, ty+t-1+1):
+        if y < 0 or y >= n: continue
+        
+        grid[y][tx] = 0
+        
+def gravity_grid():
+    
+    for x in range(n):
+        ng = [0]*n
+        next_row = 3
+        
+        for y in range(n-1, -1, -1):
+            if grid[y][x]:
+                ng[next_row] = grid[y][x]
+                next_row -= 1
+                
+        for y in range(n):
+            grid[y][x] = ng[y]
+        
+def print_grid():
+    for line in grid:
+        print(*line)
+
+for cmd in commands:
+    
+    top = -1
+    for y in range(n):
+        if grid[y][cmd]:
+            top = y
+            break
+        
+    if top == -1: continue # 열이 비었으면 다음으로
+    
+    cross_bomb(top, cmd)
+    gravity_grid()
+    
+    
+for line in grid:
+    print(*line)
