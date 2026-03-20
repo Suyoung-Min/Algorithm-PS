a04dffa11341420c841df81ce78a751114ca8d08
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-03-20 20:06:14 +0900

    Title: 2차원 폭발 게임, Time: 138ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 260320/2차원 폭발 게임/The-2D-bomb-game.py
+++ 260320/2차원 폭발 게임/The-2D-bomb-game.py
@@ -6,38 +6,36 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 def bomb(): # 같은 숫자 M 개 이상이면 터뜨리기만
     
     bomb_flag = False
-    while True:
+    
+    for x in range(n):
         
-        
-        for x in range(n):
+        last_y = 0
+        si = -1 # start_index
+        for y in range(n-1, -1, -1):
+            #breakpoint()
             
-            last_y = 0
-            si = -1 # start_index
-            for y in range(n-1, -1, -1):
-                #breakpoint()
-                
-                if grid[y][x] == 0: 
-                    last_y = y+1
-                    break
-                
-                if si == -1:
-                    si = y
-                elif grid[y][x] != grid[si][x]:
-                    
-                    if si - y >= m:
-                        bomb_flag = True
-                        for i in range(y+1, si+1):
-                            grid[i][x] = 0
-                            
-                    si = y
-
-            if si - last_y + 1 >= m: # 마지막 남은 si 가 m 이상일 때
-                bomb_flag = True
-                for i in range(last_y, si + 1):
-                    grid[i][x] = 0
-                
-            if bomb_flag == False:
+            if grid[y][x] == 0: 
+                last_y = y+1
                 break
+            
+            if si == -1:
+                si = y
+            elif grid[y][x] != grid[si][x]:
+                
+                if si - y >= m:
+                    for i in range(y+1, si+1):
+                        grid[i][x] = 0
+                    bomb_flag = True
+                        
+                si = y
+
+        if si - last_y + 1 >= m: # 마지막 남은 si 가 m 이상일 때
+            for i in range(last_y, si + 1):
+                grid[i][x] = 0
+            bomb_flag = True
+            
+    return bomb_flag
+                
 
 def gravity_fall():
     ng = [0]*n
@@ -67,8 +65,11 @@ def rotate_cw():
             grid[y][x] = ng[y][x]
             
 for _ in range(k):
-    bomb()
-    gravity_fall()
+    
+    while True:
+        if not bomb(): break
+        gravity_fall()
+        
     rotate_cw()
     gravity_fall()
     
