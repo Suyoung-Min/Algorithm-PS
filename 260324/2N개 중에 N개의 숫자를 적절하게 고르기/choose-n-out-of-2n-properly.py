27d9c6b5705b77207ff259bb11dfa81f2a7c74e0
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-03-24 20:32:11 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260324/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 260324/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -9,8 +9,6 @@ def backtracking(csum, idx, count): # 합, arr 탐색 인덱스, 현재 수열 �
     
     global ans
     
-    # 종료 조건 -> n 개 뽑았을 때, ans 보다 클 때, 인덱스 마지막일 때
-    
     if count == n:
         
         ans = min(ans, abs((all_sum - csum) - csum))
@@ -20,16 +18,12 @@ def backtracking(csum, idx, count): # 합, arr 탐색 인덱스, 현재 수열 �
     if idx == 2*n:
         return
     
-    # 두 가지 경우 - 해당 숫자를 수열에 포함시킬때, 아닐 때
-    
-    # 1. 포함
     backtracking(
         csum + arr[idx],
         idx + 1,
         count + 1
     )
     
-    # 2. 미포함
     backtracking(
         csum,
         idx + 1,
