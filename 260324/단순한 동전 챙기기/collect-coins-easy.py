5d89a721aebb8c709e78638562a15a64121cd4cf
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-03-24 20:02:46 +0900

    Title: 단순한 동전 챙기기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260324/단순한 동전 챙기기/collect-coins-easy.py
+++ 260324/단순한 동전 챙기기/collect-coins-easy.py
@@ -13,16 +13,14 @@ grid = [
 3. 동전들 중 3개 조합 -> 순서는 오름차순 고정
 4. 3개 조합 a, b, c S -> a -> b -> c -> E 거리로 최단거리 갱신
 """
-def p2p_dist(ay, ax, by, bx):
-    return abs(ay-by)+ abs(ax-bx)
+def manhattan_dist(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
 
 def solve():
-    c2c = [[0]*11 for _ in range(11)] # 각 코인별 거리 -> c2c[y][0] = S->y, c2c[y][10] -> y->E
-    coins_cord = [None for _ in range(10)] # 코인들 좌표
+    
+    coins_pos = {} # 코인들 좌표
     coins_list = []
-    S_xy = 0
-    E_xy = 0
     ans = 1e9
 
     for y in range(n):
@@ -34,68 +32,59 @@ def solve():
 
     for y in range(n):
         for x in range(n):
-            if grid[y][x].isdigit(): # 동전이면
+            if grid[y][x] == 'S': # 시작점이면
+                coins_pos['start'] = (y,x)
+            elif grid[y][x] == 'E': # 끝점이면
+                coins_pos['end'] = (y,x)
+            elif grid[y][x].isdigit(): # 동전이면
                 coin_num = int(grid[y][x])
                 
                 coins_list.append(coin_num) # 코인 저장
-                coins_cord[coin_num] = (y,x)
+                coins_pos[coin_num] = (y,x)
                 
-                c2c[coin_num][0] = p2p_dist(*(y,x), *S_xy) # S -> c 거리저장
-                c2c[coin_num][10] = p2p_dist(*(y,x), *E_xy) # c -> E 거리저장
                 
-    if len(coins_list) < 3:
-        return -1
     
     coins_list.sort()
     
-    # 각 코인별 이동거리 - 작은 코인에서 큰 코인 거리만 저장
-    
-    for i in range(len(coins_list)):
-        for j in range(i+1, len(coins_list)):
-            coin_num_a = coins_list[i]
-            coin_num_b = coins_list[j]
-            
-            c2c[coin_num_a][coin_num_b] = p2p_dist(*coins_cord[coin_num_a], *coins_cord[coin_num_b])
-            
-            
     # 코인별 조합 뽑기 및 거리 저장
     
-    def backtracking(current, start, dist): # 최대 3개
-        nonlocal ans
+    coin_nums = 0
         
-        if len(current) == 3: # 마지막 c -> E 넣어주기
+    
+    def backtracking(pos, idx, dist): # 최대 3개
+        nonlocal ans, coin_nums
+        
+        # 가지치기 pruning 넣기
+        
+        if coin_nums == 3: # 종료조건 1. 3개 뽑았을 때
             
-            dist += c2c[current[-1]][10]
+            dist += manhattan_dist(pos, coins_pos['end'])
             
             ans = min(ans, dist)
+            
             return
         
-        for i in range(start, len(coins_list)): # 코인 리스트는 오름차순 정렬되어 있음
+        if dist >= ans:
+            return
+        
+        if idx == len(coins_list): # 3개 이상으로 코인 못 뽑을 때
+            return
+        
+        
+        coin_nums += 1
+        # 위치를 동전 위치로 옮기고, 거리 누적, 다음 동전 인덱스로 진행
+        backtracking(coins_pos[coins_list[idx]], 
+                     idx+1, 
+                     dist + manhattan_dist(pos, coins_pos[coins_list[idx]]))
+        
+        coin_nums -= 1
+        
+        # 결정 2. 현재 동전(exist[idx])을 그냥 무시하고 지나칠 때
+        # 내 위치와 거리는 그대로 둔 채, 다음 동전 인덱스만 확인하러 감
+        backtracking(pos, idx + 1, dist)
+        
             
-            coin_num = coins_list[i]
-            
-            if not current: # 현재 current 가 비어있으면
-                
-                current.append(coin_num)
-                dist += c2c[coin_num][0] # S -> c
-                
-                backtracking(current, i+1, dist)
-                
-                dist -= c2c[coin_num][0]
-                current.pop()
-                
-            else: # 안비어있으면
-                
-                prev_coin_num = current[-1] # 이전 리스트 마지막 코인
-                dist += c2c[prev_coin_num][coin_num]
-                current.append(coin_num)
-                
-                backtracking(current, i+1, dist)
-                
-                dist -= c2c[prev_coin_num][coin_num]
-                current.pop()
-            
-    backtracking([], 0, 0)
+    backtracking(coins_pos['start'], 0, 0)
     
     return ans
             
