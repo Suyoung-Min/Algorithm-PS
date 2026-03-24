3dcbd29b596e0a70a63c1aa7e12d1ae1f1c83fb4
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-03-25 02:23:24 +0900

    Title: 수들의 합 최대화하기, Time: 88ms, Memory: 19MB, Status: Passed - Codetree

--- 260325/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 260325/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -3,12 +3,11 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 
-row_visited = [False] * n
-col_visited = [False] * n
+x_visited = [False] * n
 
 ans = 0
 
-def backtrack(area_sum, count): # 정수합, 정수 개수
+def backtrack(sy, area_sum, count): # start y, 정수합, 정수 개수 
     global ans
     
     # 종료 조건 n 개 뽑았을 때
@@ -16,20 +15,15 @@ def backtrack(area_sum, count): # 정수합, 정수 개수
         ans = max(ans, area_sum)
         return
     
-    for y in range(n):
-        if row_visited[y]: continue
-        for x in range(n):
-            if col_visited[x]: continue
+    for x in range(n):
+        if not x_visited[x]:
+        
+            x_visited[x] = True
             
-            row_visited[y] = True
-            col_visited[x] = True
+            backtrack(sy+1, area_sum + grid[sy][x], count+1)
             
-            backtrack(area_sum + grid[y][x], count+1)
+            x_visited[x] = False
             
-            row_visited[y] = False
-            col_visited[x] = False
-            
-            
-backtrack(0, 0)
+backtrack(0, 0, 0)
 
 print(ans)
