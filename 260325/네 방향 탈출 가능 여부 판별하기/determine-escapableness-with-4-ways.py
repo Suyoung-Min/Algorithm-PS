042fac8ab0abf218b9b4f473bfc4a59832a93f57
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-03-25 16:37:46 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 93ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260325/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+from collections import deque
+
+q = deque([])
+
+visited = [[0]*m for _ in range(n)]
+visited[0][0] = 1
+q.append((0,0))
+
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+ans = False
+
+while q:
+    y, x = q.popleft()
+    
+    if y == n-1 and x == m-1:
+        ans = True
+        break
+    
+    for dy, dx in dirs:
+        ty = y + dy
+        tx = x + dx
+        
+        if ty < 0 or ty >= n or tx < 0 or tx >= n: continue
+        
+        if not visited[ty][tx] and a[ty][tx] == 1:
+            visited[ty][tx] = True
+            q.append((ty, tx))
+            
+print(int(ans))
