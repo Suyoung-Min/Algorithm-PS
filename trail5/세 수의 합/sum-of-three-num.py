ec929165eb11693a6d2952846b2cc5e2905e1bd5
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-07-29 19:35:07 +0900

    [Passed] 세 수의 합 (339ms, 18MB)

--- trail5/세 수의 합/sum-of-three-num.py
+++ trail5/세 수의 합/sum-of-three-num.py
@@ -1,45 +1,18 @@
-n, k = map(int, input().split())
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-nd = {}
+from collections import defaultdict
+
+count = defaultdict(int)
 ans = 0
 
-for num_i in arr:
-    if num_i not in nd:
-        nd[num_i] = 1
-    else:
-        nd[num_i] += 1
+for x in arr:
+    count[x] += 1
 
-nd_items = sorted(nd.items()) 
-keys = sorted(nd.keys())
-# k:v -> num, total 숫자, 개수
-# 정렬을 통해  l <= r 을 보장
-
-for i, a in enumerate(keys):
-
-    count_a = nd[a]
-    b = k - a * 2
-
-    if a * 3 == k: # A == A == A
-        ans += max(nd[a] * (nd[a] - 1) * (nd[a] - 2) // 6, 0)
-    elif b in nd and a < b: # A == A < B
-        ans += nd[a] * (nd[a] - 1) // 2 * nd[b]
-
-    for j in range(i+1, len(keys)):
-
-        b = keys[j]
-        count_b = nd[b]
-
-        c = k - (a + b)
-
-        if b > c: break
-
-        # A < B == C
-        if b == c:
-            ans += max(nd[a] * nd[b] * (nd[b] - 1) // 2, 0)
-        # A < B < C
-        elif c in nd:
-            ans += nd[a] * nd[b] * nd[c]
+for i in range(n):
+    count[arr[i]] -= 1
+    for j in range(i):
+        ans += count.get(k-arr[i]-arr[j], 0)
 
 print(ans)
