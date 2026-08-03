3abe35ceecf764d1ed698366cd9b6554971e02f9
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-03 13:13:08 +0900

    [Passed] 정점 연결하기 (479ms, 44MB)

--- trail6/정점 연결하기/connect-vertices.py
+++ trail6/정점 연결하기/connect-vertices.py
@@ -3,33 +3,25 @@ edges = [tuple(map(int, input().split())) for _ in range(n - 2)]
 
 # Please write your code here.
 
-parent = [i for i in range(n+1)] # 1 ~ n 정점
+parent = [i for i in range(n+1)] # 1~n
+
 
 def find(x):
-    if parent[x] != x : # 루트 노드가 아닐 때 / 부모가 있을 때
-        parent[x] = find( parent[x] )
+    if x == parent[x]: return x
+    parent[x] = find(parent[x])
     return parent[x]
+
+def same(a, b):
+    return find(a) == find(b)
 
 def union(a, b):
     root_a = find(a)
     root_b = find(b)
     
-    parent[ max(root_a, root_b) ] = min(root_a, root_b)
-    
-def same(a, b):
-    return find(a) == find(b)
+    parent[max(root_a, root_b)] = min(root_a, root_b)
 
-for u, v in edges:
-    union(u, v)
+for s, e in edges:
+    union(s, e)
     
-root_node_1, root_node_2 = int(1e10), int(1e10)
-for i in range(1, n+1):
-    tp = find(i)
-    
-    if tp < root_node_1 and tp < root_node_2:
-        root_node_1 = tp
-    elif root_node_1 < tp and tp < root_node_2:
-        root_node_2 = tp
-        
-        
-print(root_node_1, root_node_2)
+roots = sorted({find(i) for i in range(1, n+1)})
+print(roots[0], roots[1])
