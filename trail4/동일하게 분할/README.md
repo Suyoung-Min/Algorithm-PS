42cf190b2350196533f002431fed64257d919ab7
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-05 15:58:16 +0900

    [Passed] 동일하게 분할 (50ms, 16MB)

---
+++ trail4/동일하게 분할/README.md
@@ -0,0 +1,8 @@
+# [[테스트]동일하게 분할](https://www.codetree.ai/trails/complete/curated-cards/test-equal-partition)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 4 / DP I / 아이템을 적절히 고르는 문제](https://www.codetree.ai/trail-info/intermediate-low/) |
+| 난이도 | 쉬움 |
+| 경험치 | 60 XP |
---
+++ trail4/동일하게 분할/equal-partition.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+if sum(arr)%2: # 전체합이 짝수가 아니면
+    print('No')
+    exit(0)
+
+m = sum(arr)//2
+
+dp = [0] * (m+1)
+dp[0] = 1
+
+# dp[x] 합이 x 인 부분수열의 존재여부
+
+# 원소 하나씩 / 내림차순
+# 순서 없이 -> 조합 / for a in arr 바깥쪽
+
+for a in arr:
+    for i in range(m, a-1, -1):
+        dp[i] |= dp[i-a]
+        
+print('Yes' if dp[m] else 'No')
