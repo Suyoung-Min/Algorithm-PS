4c6a4f00dd1a0f496af1d8377b3938f4197f07b3
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-05 11:20:13 +0900

    [Passed] 최대 동전 거슬러주기 (62ms, 17MB)

--- trail4/최대 동전 거슬러주기/max-coin-change.py
+++ trail4/최대 동전 거슬러주기/max-coin-change.py
@@ -4,10 +4,9 @@ coin = list(map(int, input().split()))
 
 dp = [-1]*(m+1)
 dp[0] = 0
-MOD = 10007
 
-for c in coin:
-    for i in range(m+1):
+for i in range(m+1):
+    for c in coin:
         if i - c >= 0 and dp[i-c] != -1:
             dp[i] = max(dp[i], dp[i-c] + 1)
             
