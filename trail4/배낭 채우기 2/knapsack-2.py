ae9dc506fadafefb768b9d4f46183fca9ffff973
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-05 13:17:57 +0900

    [Passed] 배낭 채우기 2 (55ms, 17MB)

---
+++ trail4/배낭 채우기 2/knapsack-2.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+jewel = [tuple(map(int, input().split())) for _ in range(n)] # w v
+
+# 순서 무시, 무제한
+# jewel 바깥쪽, 오름차순
+
+# dp[x] 배낭 무게가 x 일 때 최대 가치
+dp = [-1] * (m+1)
+dp[0] = 0
+
+for w, v in jewel:
+    for x in range(w, m+1):
+        if dp[x - w] != -1: # 갱신된 배낭일 때
+            dp[x] = max(dp[x], dp[x - w] + v)
+            
+print(max(dp))
