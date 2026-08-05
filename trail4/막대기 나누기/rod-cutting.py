7961589cb31d63a1fa551b9ffaabbfaffcf83559
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-05 12:54:55 +0900

    [Passed] 막대기 나누기 (47ms, 16MB)

--- trail4/막대기 나누기/rod-cutting.py
+++ trail4/막대기 나누기/rod-cutting.py
@@ -2,12 +2,12 @@ n = int(input())
 
 stick = [0] + list(map(int, input().split()))
 
-dp = [-1] * (n + 1)
-dp[0] = 0
+# dp[x] 길이를 x 까지 쓸 때 최대 이익
+dp = [0] * (n + 1)
 
 for s in range(1, n+1): # 길이
     for i in range(s, n+1):
-        if dp[i - s] != -1:
-            dp[i] = max(dp[i], dp[i - s] + stick[s])
+        # if dp[i - s] != -1: 길이 1도 있으니 항상 갱신됨
+        dp[i] = max(dp[i], dp[i - s] + stick[s])
             
 print(max(dp))
