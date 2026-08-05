ad2f6396578a70b544c324d0b10dad0495096bdc
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-05 10:40:37 +0900

    [Passed] 1, 2, 5 더하기 (57ms, 17MB)

--- trail4/1, 2, 5 더하기/1-2-5-plus.py
+++ trail4/1, 2, 5 더하기/1-2-5-plus.py
@@ -5,14 +5,9 @@ dp[0] = 1
 MOD = 10007
 
 for i in range(1, n+1):
-    dp[i] += dp[i-1]
-    
-    if i >= 2:
-        dp[i] += dp[i-2]
-        
-    if i >= 5:
-        dp[i] += dp[i-5]
-        
-    dp[i] %= MOD
+    for c in (1, 2, 5):
+        if i - c >= 0:
+            dp[i] += dp[i - c]
+            dp[i] %= MOD
     
 print(dp[n])
