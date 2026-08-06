71b28a89cec727c5cc1155f7986739270b5042a8
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-06 14:28:04 +0900

    [Passed] 최소 스패닝 트리 8 (83ms, 17MB)

--- trail6/최소 스패닝 트리 8/minimum-spanning-tree-8.py
+++ trail6/최소 스패닝 트리 8/minimum-spanning-tree-8.py
@@ -2,32 +2,42 @@ n, m = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
 
+parent = [i for i in range(n+1)] # 1 ~ n 부모 자기자신으로 초기화
+
+def find(x):
+    if x == parent[x]: return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def same(a, b):
+    return find(a) == find(b)
+
+def union(a, b):
+    root_a = find(a)
+    root_b = find(b)
+    
+    parent[ max(root_a, root_b) ] = min(root_a, root_b)
+
+edges = []
+
 for _ in range(m):
     s, e, w = map(int, input().split())
     
-    graph[s].append((e, w))
-    graph[e].append((s, w))
+    edges.append((w, s, e))
     
-import heapq
-
-visited = [False] * (n+1)
-
-start = 1
-q = [(0, start)] # cost, v
+edges.sort()
 
 weight_sum = 0
+edges_num = 0
 
-
-while q:
-    cost, v = heapq.heappop(q)
+for w, s, e in edges:
     
-    if visited[v]: continue
-    visited[v] = True
-    weight_sum += cost
+    if same(s, e): continue
     
+    union(s, e)
+    weight_sum += w
+    edges_num += 1
     
-    for next_v, next_cost in graph[v]:
-        if not visited[next_v]:
-            heapq.heappush(q, (next_cost, next_v))
-            
-print(weight_sum)
+    if edges_num == n-1:
+        print(weight_sum)
+        break
