28f0fba5e1a197a9bf91a9ad2d32bb56d6011773
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-18 12:56:02 +0900

    [Passed] 최대공약수 구하기 (2ms, 0MB)

---
+++ trail2/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <numeric>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,m;
+    cin>>n>>m;
+    cout<<gcd(n,m)<<endl;
+    return 0;
+}
