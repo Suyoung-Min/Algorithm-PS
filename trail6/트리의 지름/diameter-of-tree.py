a5dcd5e975795ad3c9eefa327ac789da1d7b4850
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-19 13:54:57 +0900

    [Passed] 트리의 지름 (502ms, 38MB)

--- trail6/트리의 지름/diameter-of-tree.py
+++ trail6/트리의 지름/diameter-of-tree.py
@@ -12,33 +12,24 @@ def solve():
 
     start_node = 1
 
-    q = deque([1])
-    distance = [float('inf')] * (n + 1)
-    distance[1] = 0
+    def bfs(start_node):
+        q = deque([start_node])
+        distance = [float('inf')] * (n+1)
+        distance[start_node] = 0
 
-    while q:
-        x = q.popleft()
+        while q:
+            x = q.popleft()
 
-        for y, next_d in edges[x]:
-            if distance[x] + next_d < distance[y]:
-                distance[y] = distance[x] + next_d
-                q.append(y)
+            for y, next_d in edges[x]:
+                if distance[x] + next_d < distance[y]:
+                    distance[y] = distance[x] + next_d
+                    q.append(y)
 
+        return distance
+
+    distance = bfs(1)
     second_start = distance.index(max(distance[1:]))
-    distance = [float('inf')] * (n+1)
-    distance[second_start] = 0
-    q = deque([second_start])
-
-    while q:
-        x = q.popleft()
-
-        for y, next_d in edges[x]:
-            if distance[x] + next_d < distance[y]:
-                distance[y] = distance[x] + next_d
-                q.append(y)
-
+    distance = bfs(second_start)
     print(max(distance[1:]))
-
-
 
 solve()
