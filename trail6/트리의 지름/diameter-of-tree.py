12a8bd53d6f7699aca406b6f600a4b5eff1249e8
Suyoung Min <65070885+Suyoung-Min@users.noreply.github.com>
2026-08-19 13:01:33 +0900

    [Passed] 트리의 지름 (534ms, 39MB)

--- trail6/트리의 지름/diameter-of-tree.py
+++ trail6/트리의 지름/diameter-of-tree.py
@@ -1,45 +1,44 @@
-import sys
+def solve():
+    from collections import deque
+    n = int(input())
 
-sys.setrecursionlimit(10**6)
+    edges = [[] for _ in range(n + 1)]
 
-n = int(input())
-edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
+    for _ in range(n - 1):
+        s, e, d = map(int, input().split())
 
-# Please write your code here.
+        edges[s].append((e, d))
+        edges[e].append((s, d))
 
-# 2. 노드가 1개일 경우 지름은 0이므로 바로 종료합니다.
-if n == 1:
-    print(0)
-    sys.exit()
+    start_node = 1
 
-tree = [[] for _ in range(n+1)]
-parent = [0] * (n + 1)
-dist = [0] * (n + 1)
+    q = deque([1])
+    distance = [float('inf')] * (n + 1)
+    distance[1] = 0
 
-for u, v, e in edges:
-    tree[u].append((e, v))
-    tree[v].append((e, u))
+    while q:
+        x = q.popleft()
 
-def dfs(node):
-    for distance, child in tree[node]:
-        if parent[child] == 0: # 아직 방문안한 노드라면
-            parent[child] = node
-            dist[child] = dist[node] + distance
-            dfs(child)
+        for y, next_d in edges[x]:
+            if distance[x] + next_d < distance[y]:
+                distance[y] = distance[x] + next_d
+                q.append(y)
+
+    second_start = distance.index(max(distance[1:]))
+    distance = [float('inf')] * (n+1)
+    distance[second_start] = 0
+    q = deque([second_start])
+
+    while q:
+        x = q.popleft()
+
+        for y, next_d in edges[x]:
+            if distance[x] + next_d < distance[y]:
+                distance[y] = distance[x] + next_d
+                q.append(y)
+
+    print(max(distance[1:]))
 
 
-parent[1] = 1 # 시작점이니 부모는 없다 -> 자기자신
-dfs(1)#
 
-st_pos = dist.index(max(dist)) 
-# 1. 거리가 최댓값인 노드를 시작점으로 
-# 2. 시작점에서 다시 거리가 최대인 점과의 거리 계산
-
-# tree 는 그대로, parent, dist 다시 초기화
-parent = [0] * (n + 1)
-dist = [0] * (n + 1)
-
-parent[st_pos] = st_pos # 자기자신을 부모로 초기화
-dfs(st_pos)
-
-print(max(dist)) # 이후 최댓값이 트리의 지름
+solve()
